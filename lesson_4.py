ba536aa51b4892860ec7dc7a9d71fbc324648022
Super <pozharsky2020@gmail.com>
2022-09-28 18:19:55 +0200

    change code example

--- lesson_4.py
+++ lesson_4.py
@@ -1,173 +1,92 @@
-# тип bool, None
-# приведение типов
-# оператор if
+# функция input
+# обработка ошибок
+# тернарный оператор
+# цикл с условием
+# методы строк
 
-##################################################################
+value = input("Введи целое число: ")
+try:
+    value = int(value)
+    result = 1 / value
+    print(value, result)
+except (ValueError, TypeError):
+    print("Это не целое число!!!")
+except ZeroDivisionError:
+    print("На ноль делить нельзя!!!")
 
-my_strange_value = None
-print(my_strange_value, type(my_strange_value))
 
-value_1 = 3
-value_2 = 4
-result = print(value_1 + value_2)
-print(result)
+################ тернарный оператор
 
-##################################################################
-
-# my_bool_value = False
-# print(my_bool_value, type(my_bool_value))
-
-# my_bool_value = 10 == 2
-## <, >, <=, >=, ==, !=
-## not
-# new_value = not my_bool_value
-# print(my_bool_value, new_value)
-
-value_1 = True
-value_2 = True
-result = value_1 and value_2  # логическое И - муж пьет чай, а жена кофе
-
-print(result)
-
-value_1 = False
-value_2 = False
-result = value_1 or value_2  # логическое ИЛИ - Я пью чай или кофе
-
-print(result)
-
-number = 6
-color = "red"
-
-result = (number >= 5 or
-          not number == 1 and
-          color == 'red')
-
-print(result)
-
-#################################
-
-value = 123
-
-if value > 100:
-    print(f"Value more than 100")
+value = -12
+# #################
+if value >= 0:
+    result = value
 else:
-    print(f"Value less or equal than 100")
+    result = value * 2
+print(result)
+# #################
+result = value if value >= 0 else value * 2
+print(result)
+# новая_переменная = знач_если_да if условие else знач_если_нет
 
-# if условие:
-#     # блок, если ДА
-# elif условие_2:
-#     # блок, если ДА
-#  ...................
-# else:
-#     # блок, если НЕТ
-
-value = 0
-if value > 0:
-    print(f"Value is positive number")
-elif value < 0:
-    print(f"Value is negative number")
-else:
-    print(f"Value is 0")
-
-#####################################################
-# приведение типов
-
-value_int = 10
-value_float = float(value_int)
-print(value_float, type(value_float), value_int, type(value_int))
+########################## цикл с условием
 
 value = 10
-value = float(value)
-print(value, type(value))
+while value >= 0:
+    print(value)
+    value -= 1
 
-value = -10.9
-value = int(value)
-print(value, type(value))
+## не очень хорошее решение
+while True:
+    print(value)
+    value -= 1
+    if value < 0:
+        break
 
-value = -10
-value = str(value)
-print(value, type(value))
+# решение под конкретную ситуацию
+do_while = True
+while do_while:
+    print(value)
+    value -= 1
+    if value < 0:
+        do_while = False
 
-value = "100_0_00"
-value = int(value)
-print(value, type(value))
 
-value = "-100.078"
-value = float(value)
-print(value, type(value))
-
-value = None
-value = str(value)
-print(value, type(value))
-
-value = -10   # int -> bool True, кроме 0
-value = bool(value)
-print(value, type(value))
-
-value = 0.0000000000000001   # float -> bool True, кроме 0.0
-value = bool(value)
-print(value, type(value))
-
-value = ""   # str -> bool True, кроме ""     !!!!!!!
-value = bool(value)
-print(value, type(value))
-
-value = None   # None -> bool всегда False     !!!!!!!
-value = bool(value)
-print(value, type(value))
+############################### методы строк
 
 my_str = "qwerty"
-number = 123
-my_value = str(number)
-print(my_value, type(my_value))
+index = 0
+index = -1  # отсчет от конца строки
+symbol = my_str[index]  # получение значения по индексу
+print(symbol)
 
+if index < len(my_str):
+    symbol = my_str[index]  # получение значения по индексу
+    print(symbol)
 
-value = None
+symbol_count = len(my_str)
+print(symbol_count)
 
-if not value:
-    print("Value is None")
+my_str[0] = "Q"  # TypeError: 'str' object does not support item assignment
 
-if value:
-    print(f"This value {value} is not 0")
+#   Срезы строк
+my_str = "qwerty"
+new_str = my_str[1:5]  # от - включено, до - исключено
+new_str = my_str[3:]  # от .. и до конца
+new_str = my_str[:-3]  # от начала и до ...
+new_str = my_str[:]
+new_str = my_str[-40:20]
+print(new_str)
 
-#####################################################
-## in
+new_str = "Q" + my_str[1:]
+new_str = my_str[:3] + "R" + my_str[4:]
+print(new_str)
 
-my_str = "<SJHDGfj<SHVMGXJzxcvJSDCHALSJhjSGVC"
-sub_str = 'zxc'
+new_str = my_str[::2]  # 2 - шаг среза. Значения на четных индексах
+new_str = my_str[1::2]  # 2 - шаг среза. Значения на нечетных индексах
+new_str = my_str[::-1]  # разворот строки
+print(new_str)
 
-if sub_str in my_str:
-    print("!!!!!!!!!!!!!")
-
-
-value_1 = 19
-value_2 = 2
-
-if value_1 % 2 == 0:
-    if value_2 > 0:
-        print("Case 1")
-    else:
-        print("Case 2")
-else:
-    print("Case 3")
-
-#####################################################
-value_1 = 11
-
-if value_1 % 2 == 0 and value_1 % 3 == 0:
-    print("Делится на 6")
-elif value_1 % 2 == 0:
-    print("Делится на 2")
-elif value_1 % 3 == 0:
-    print("Делится на 3")
-else:
-    print("Не делится ни на 2 ни на 3")
-#####################################################
-if value_1 % 2 == 0:
-    print("Делится на 2")
-else:
-    print("НЕ Делится на 2")
-if value_1 % 3 == 0:
-    print("Делится на 3")
-else:
-    print("Не делится на 3")
+my_str = 'asD'
+if my_str[-1].isupper():  # метод строки
+    print("This is number", my_str)
